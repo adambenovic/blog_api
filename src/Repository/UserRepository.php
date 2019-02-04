2e188ea5113e9b27e3fe89b4a085a35b5086ea89
Adam Benovic <adam.benovic@bootiq.io>
2019-02-04 15:52:39 +0100

    changed to PostgreSQL

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class UserRepository extends BaseRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+    /**
+     * @param int $id ID of user to get
+     * @return User The user from DB
+     */
+    public function getUserByID(int $id): User
+    {
+        try
+        {
+            $user = $this->createQueryBuilder('a')
+                ->andWhere('a.id= :val')
+                ->setParameter('val', $id)
+                ->getQuery()
+                ->getOneOrNullResult()
+            ;
+        }
+        catch (\Doctrine\ORM\NonUniqueResultException $ex)
+        {
+            echo "User not unique. NEVER HAPPENS";
+        }
+        return $user;
+    }
+}
